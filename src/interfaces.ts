1e269a9a5c184b84c2f9a628198dbfb52e27e41a
horitomoyuki <eziltom0131@gmail.com>
2022-02-07 21:25:26 +0900

    Create.interfaces.ts

---
+++ src/interfaces.ts
@@ -0,0 +1,16 @@
+export {};
+
+type ObjetcType = {
+  name: string;
+  age: number;
+};
+
+interface ObjectInterface {
+  name: string;
+  age: number;
+}
+
+let object: ObjectInterface = {
+  name: 'Tom',
+  age: 100
+};
